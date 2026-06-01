04a0c9263f4d166f3c45c12addc3eebfdbf362b4
Manish <mkk725050@gmail.com>
2026-06-01 14:13:04 +0530

    Using Function I checked Even Or Odd

---
+++ EvenOddCheck.dart
@@ -0,0 +1,15 @@
+void checkEvenOdd(int num) {
+
+  if (num % 2 == 0) 
+  {
+    print("Number Is Even");
+  } 
+  else 
+  {
+    print("Number Is Odd");
+  }
+}
+
+void main() {
+  checkEvenOdd(3);
+}
---
+++ Square.dart
@@ -0,0 +1,7 @@
+void square(int num) {
+  print(num * num);
+}
+
+void main() {
+  square(2);
+}
