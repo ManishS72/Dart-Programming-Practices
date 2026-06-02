9d43735f1f5aff28985ce3c6f8a8d8cbb69f3590
Manish <mkk725050@gmail.com>
2026-06-02 12:04:37 +0530

    Employee add salry

---
+++ Employee.dart
@@ -0,0 +1,13 @@
+void main() {
+  Map<String, dynamic> employee = {
+
+    "name": "Rahul", 
+    "id": "101"
+    
+    };
+
+      employee["name"] = "Manish";
+      employee["salary"] = "30000";
+
+      print(employee);
+}
