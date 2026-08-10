c3809a67c3813931381781de264d247623bad783
Manish <mkk725050@gmail.com>
2026-08-10 19:43:39 +0530

    added name, age, salary

--- Hello.dart
+++ Hello.dart
@@ -1,3 +1,18 @@
 void main() {
   print("Hello, From Today I started to Learnd Dart Internship Levels");
+
+  //Variables
+
+  String name = "Manish";
+  int age = 25;
+  double salary = 50000;
+  bool isDeveloper = true;
+
+  print(name);
+  print(age);
+  print(salary);
+  print(isDeveloper);
+
 }
+
+// Variables Start from Here !
