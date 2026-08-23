a5071d0fffdcfeef8b4f2a729a07b1ea68eef1a3
Manish <mkk725050@gmail.com>
2026-08-23 13:46:02 +0530

    Practies

---
+++ Ma.dart
@@ -0,0 +1,13 @@
+void main() {
+  String name = "Manish Kumar";
+  int age = 25;
+  double height = 5.7;
+  double salary = 58000;
+  bool isFlutterDeveloper = true;
+
+  print(name);
+  print(age);
+  print(height);
+  print(salary);
+  print(isFlutterDeveloper);
+}
